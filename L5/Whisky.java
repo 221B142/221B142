5ba0149a87b61df47707ac365c73e83836121043
221B142 <221B142@juet.co.in>
2025-03-25 12:33:42 +0530

    lab 4

--- L5/Whisky.java
+++
@@ -1,8 +0,0 @@
-class Whisky extends Beverage{
-    public void addCondiments(){
-        System.out.println("add Soda");
-    }
-    public void Stir(){
-        System.out.println("Stir for 30 seconds");
-    }
-}
